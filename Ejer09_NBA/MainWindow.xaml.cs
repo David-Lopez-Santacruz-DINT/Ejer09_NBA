9081a2d9ef0e5a6bf1c39398117c8a7d4b1017d4
abc <abc@example.com>
2020-11-03 18:40:25 +0100

    Agregar archivos de proyecto.

---
+++ Ejer09_NBA/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Ejer09_NBA
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            letraTextBlock.Foreground = new SolidColorBrush(Colors.White);
+        }
+    }
+}
